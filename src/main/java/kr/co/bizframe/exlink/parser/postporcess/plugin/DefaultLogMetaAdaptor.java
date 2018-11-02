2d5eb160f5b3562f2e6d263cc3d0714bae7437d9
sushin <sushin@torpedo.co.kr>
2018-11-02 17:16:53 +0900

    파싱 후 전체 데이터 후처리 어뎁터 추가

---
+++ src/main/java/kr/co/bizframe/exlink/parser/postporcess/plugin/DefaultLogMetaAdaptor.java
@@ -0,0 +1,50 @@
+package kr.co.bizframe.exlink.parser.postporcess.plugin;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import kr.co.bizframe.exlink.ExlinkDslException;
+import kr.co.bizframe.exlink.model.ExFileInfo;
+import kr.co.bizframe.exlink.parser.conf.FileParserConfingFactory;
+import kr.co.bizframe.exlink.parser.conf.model.FileParserMapConf.MapField;
+import kr.co.bizframe.exlink.parser.conf.model.FileParserMetaConf;
+import kr.co.bizframe.exlink.parser.conf.model.FileParserMetaConf.FileParserMapConfInfo;
+import kr.co.bizframe.exlink.parser.postporcess.AbstractPostMapItemAdaptor;
+import kr.co.bizframe.exlink.parser.postporcess.AbstractPostMetaAdaptor;
+import kr.co.bizframe.exlink.sql.SqlExecutor;
+import kr.co.bizframe.exlink.sql.scirpt.SqlParser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DefaultLogMetaAdaptor extends AbstractPostMetaAdaptor{
+	
+	public static Logger logger = LoggerFactory.getLogger(DefaultLogMetaAdaptor.class);
+	
+	public DefaultLogMetaAdaptor() {
+		
+	}
+
+	@Override
+	public void process(FileParserMetaConf metaConf, AbstractMap metaData, ExFileInfo info) throws ExlinkDslException {
+		int totalRowCount = 0;
+		for(Object key :metaData.keySet()){ 
+			List mapDatas = (List) metaData.get(key);
+			FileParserMapConfInfo fMapInfo = metaConf.getMapConfByMapKey((String)key);
+			int keyCount = mapDatas.size();
+			totalRowCount = totalRowCount + keyCount;
+			//logger.debug("▶▶▶▶▶▶▶▶▶ key="+key+" key row count=["+keyCount+"]  total row count=["+totalRowCount+"]");
+		}
+	}
+
+
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/kr/co/bizframe/exlink/parser/postporcess/plugin/DefaultWriteDBMapItemAdaptor.java
@@ -0,0 +1,70 @@
+package kr.co.bizframe.exlink.parser.postporcess.plugin;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import kr.co.bizframe.exlink.ExlinkDslException;
+import kr.co.bizframe.exlink.parser.conf.FileParserConfingFactory;
+import kr.co.bizframe.exlink.parser.conf.model.FileParserMapConf.MapField;
+import kr.co.bizframe.exlink.parser.conf.model.FileParserMetaConf.FileParserMapConfInfo;
+import kr.co.bizframe.exlink.parser.postporcess.AbstractPostMapItemAdaptor;
+import kr.co.bizframe.exlink.sql.SqlExecutor;
+import kr.co.bizframe.exlink.sql.scirpt.SqlParser;
+import kr.co.bizframe.exlink.type.AppendColumnType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DefaultWriteDBMapItemAdaptor extends AbstractPostMapItemAdaptor{
+	
+	public static Logger logger = LoggerFactory.getLogger(DefaultWriteDBMapItemAdaptor.class);
+	
+	private static HashMap<String ,String> sqlMap = new HashMap<String ,String>();
+
+	public DefaultWriteDBMapItemAdaptor() {
+		
+	}
+
+	@Override
+	public void process(AbstractMap<String, Object> params, FileParserMapConfInfo conf, String key)
+			throws ExlinkDslException {
+		long startTime  = System.currentTimeMillis();
+		String sql_scirpt =  sqlMap.get(conf.getMapConf().getConfId());
+		
+		if(sql_scirpt == null){
+			String header = "INSERT INTO "+conf.getMapConf().getTableName() + " (";
+			String column = "";
+			String values = "";
+			
+			for(MapField field : conf.getMapConf().getFields()){
+				column = column +(column.equals("")? field.getNameEng():", "+field.getNameEng());
+				values= values + (values.equals("")? "#"+field.getNameEng():" ,#"+field.getNameEng());
+			}
+			if(params.get(AppendColumnType.IF_FILE_NAME.getCode()) != null){
+				column = column + ", "+AppendColumnType.IF_FILE_NAME.getCode();
+				values= values + " ,#"+AppendColumnType.IF_FILE_NAME.getCode();
+			}
+			
+			if(params.get(AppendColumnType.IF_SYNC_YN.getCode()) != null){
+				column = column + ", "+AppendColumnType.IF_SYNC_YN.getCode();
+				values= values + " ,#"+AppendColumnType.IF_SYNC_YN.getCode();
+			}
+			sql_scirpt = header + column+" ) VALUES ("+values+" )";
+			sqlMap.put(key, sql_scirpt);
+		}
+		SqlParser parser = new SqlParser();
+		String runSql = parser.getQuery(sql_scirpt);
+		//logger.debug(runSql);
+		SqlExecutor sqlExecutor = new SqlExecutor();
+		sqlExecutor.executeSql(runSql, parser.getColumnList(), FileParserConfingFactory.getInstance().getDatasource(), params);
+		long endTime  = System.currentTimeMillis();
+		//logger.debug("insert time ="+(endTime-startTime));
+		
+	}
+	
+	
+	
+	
+	
+}
